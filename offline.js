9cbd56e468b267bb3d773ac199477eff757e069d
arfeight <161670063+arfeight@users.noreply.github.com>
2024-03-01 08:20:33 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709167348,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgrsheet-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/bwhsheet-sheet0.png",
+		"images/atassheet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
